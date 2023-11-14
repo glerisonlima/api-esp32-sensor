96db3d743f1eb00c68d9e9bf0dfb1561c97798aa
Glerison Lima <glerisonlima@gmail.com>
2023-11-14 09:34:34 -0300

    Create server.ino

---
+++ server.ino
@@ -0,0 +1,81 @@
+#include <WiFi.h>
+
+const char* ssid     = "ESTACIO-VISITANTES";
+const char* password = "estacio@2014";
+
+const int PIN_SENSOR = 13;
+
+WiFiServer server(80);
+
+void setup()
+{
+    Serial.begin(115200);
+    pinMode(PIN_SENSOR, INPUT_PULLUP); /* Lê um estado HIGH quando a porta está fechada e LOW quando a porta estiver aberta */
+    delay(10);
+
+    // We start by connecting to a WiFi network
+
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected.");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    
+    server.begin();
+
+}
+
+void loop(){
+ WiFiClient client = server.available();   // listen for incoming clients
+
+  if (client) {                             // if you get a client,
+    Serial.println("New Client.");           // print a message out the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        if (c == '\n') {                    // if the byte is a newline character
+
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/json");
+            client.println("Access-Control-Allow-Origin: *");
+            client.println();
+
+            if(digitalRead(PIN_SENSOR) == 1){
+              client.println("aberta");
+            }else{
+              client.println("fechada");
+            }
+            // The HTTP response ends with another blank line:
+            client.println();
+            // break out of the while loop:
+            break;
+          } else {    // if you got a newline, then clear currentLine:
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // close the connection:
+    client.stop();
+    Serial.println(digitalRead(PIN_SENSOR));
+    Serial.println("Client Disconnected.");
+  }
+}
